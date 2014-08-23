fed05f43e81fcd59b242758a922262b56b6c3ecd
panesofglass <ryan.riley@panesofglass.org>
2014-08-23 15:04:33 -0500

    Implement todo storage using MailboxProcessor.

---
+++ TodoMvcFSharp/Startup.fs
@@ -0,0 +1,20 @@
+﻿namespace TodoMvcFSharp
+
+open Owin
+open Microsoft.Owin
+open System.Net.Http
+open System.Web.Http
+
+type Startup() =
+
+    static member RegisterWebApi(config: HttpConfiguration) =
+        // Configure serialization
+        config.Formatters.XmlFormatter.UseXmlSerializer <- true
+        config.Formatters.JsonFormatter.SerializerSettings.ContractResolver <- Newtonsoft.Json.Serialization.CamelCasePropertyNamesContractResolver()
+
+    member __.Configuration(app: IAppBuilder) =
+        app .UseCors(Cors.CorsOptions.AllowAll)
+            |> ignore
+
+[<assembly: OwinStartupAttribute(typeof<Startup>)>]
+do ()
